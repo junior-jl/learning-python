3a258b0a0b1d90317c853fcc18c50de8ae88a62d
José Lira Junior <69206952+junior-jl@users.noreply.github.com>
2022-07-20 16:03:48 -0300

    Update animal_details.py

--- object-oriented-programming/exercises/animal_details.py
+++ object-oriented-programming/exercises/animal_details.py
@@ -54,3 +54,37 @@ class Sheep(Animal):
         print("Name: {}".format(self.name))
         print("Sound: {}".format(self.sound))
         print("Color: {}".format(self.color))
+
+        
+        
+# Educative solution was better, since it uses the overridden (super().Animal_details()) method to avoid redundance.
+
+class Animal:
+    def __init__(self, name, sound):
+        self.name = name
+        self.sound = sound
+
+    def Animal_details(self):
+        print("Name:", self.name)
+        print("Sound:", self.sound)
+
+
+class Dog(Animal):
+    def __init__(self, name, sound, family):
+        super().__init__(name, sound)
+        self.family = family
+
+    def Animal_details(self):
+        super().Animal_details()
+        print("Family:", self.family)
+
+
+class Sheep(Animal):
+    def __init__(self, name, sound, color):
+        super().__init__(name, sound)
+        self.color = color
+
+    def Animal_details(self):
+        super().Animal_details()
+        print("Color:", self.color)
+

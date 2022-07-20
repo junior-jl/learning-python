aeebe996232f26255338407bab5e41ee30c71419
José Lira Junior <69206952+junior-jl@users.noreply.github.com>
2022-07-20 17:04:07 -0300

    Create sedan.py

---
+++ object-oriented-programming/exercises/sedan.py
@@ -0,0 +1,34 @@
+# You have to implement a Sedan class, which inherits from the Car class and contains a SedanEngine object.
+# Note: You already know that in such a composition relation, the Sedan class will be responsible for SedanEngine lifetime.
+
+# My solution
+
+class Car:
+    def __init__(self, model, color):
+        self.model = model
+        self.color = color
+    
+    def printDetails(self):
+        print("Model: {}".format(self.model))
+        print("Color: {}".format(self.color))
+
+
+class SedanEngine:
+    def start(self):
+        print("Car has started.")
+
+    def stop(self):
+        print("Car has stopped.")
+
+
+class Sedan(Car):
+    def __init__(self, model, color):
+        super().__init__(model, color)
+        self.engine = SedanEngine()
+
+    def setStart(self):
+        self.engine.start()
+
+    def setStop(self):
+        self.engine.stop()
+        

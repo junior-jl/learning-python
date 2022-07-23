96ed5b68c09356dbbf065c92c2ec2962a1a48354
José Lira Junior <69206952+junior-jl@users.noreply.github.com>
2022-07-22 21:23:56 -0300

    Create tuple_from_list.py

---
+++ data-structures-and-algorithms/exercises/tuple_from_list.py
@@ -0,0 +1,8 @@
+# You are given a list called my_list. Using this list, you must create a tuple called my_tuple. The tuple will contain the list’s first element,
+# last element, and the length of the list, in that same order.
+
+# My solution
+
+my_tuple = (my_list[0], my_list[len(my_list) - 1], len(my_list))
+
+# Educative solution was the same.
